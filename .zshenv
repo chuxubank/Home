1cab57bab6dafd8b00f575864682e962d1c7c70d
Asuna <chuxubank@qq.com>
2020-02-17 10:38:20 +0800

    Remove llvm settings

--- .zshenv
+++ .zshenv
@@ -4,11 +4,5 @@ export PATH="$PATH:$HOME/Developer/.flutter/bin"
 # rime
 export RIME_PATH="$HOME/Developer/.librime"
 
-# llvm
-# use brew llvm first
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/llvm/lib"
-export CPPFLAGS="-I/usr/local/opt/llvm/include"
-
 # doom-emacs
 export PATH="$HOME/.doom/bin:$PATH"
--- mUseUniversalHeaderFile.sh
+++ mUseUniversalHeaderFile.sh
@@ -1,4 +1,4 @@
 wget -P ~/Downloads https://gist.githubusercontent.com/chuxubank/378162ea500aa54738f1068c2dfa8933/raw/stdc++.h
-sudo cp ~/Downloads/stdc++.h /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/bits/
-sudo cp ~/Downloads/stdc++.h /usr/local/opt/llvm/include/c++/v1/bits/
+cd /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/
+sudo rsync ~/Downloads/stdc++.h bits/
 rm ~/Downloads/stdc++.h
