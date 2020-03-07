970d20dbbc8cd7796cbae18d97428ce94d907504
Asuna <chuxubank@qq.com>
2020-03-07 20:03:35 +0800

    Revert "Remove llvm settings"
    This reverts commit 1cab57bab6dafd8b00f575864682e962d1c7c70d.

--- .zshenv
+++ .zshenv
@@ -6,6 +6,12 @@ export PUB_HOSTED_URL=https://mirrors.tuna.tsinghua.edu.cn/dart-pub
 # rime
 export RIME_PATH="$HOME/Developer/.librime"
 
+# llvm
+# use brew llvm first
+export PATH="/usr/local/opt/llvm/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/llvm/lib"
+export CPPFLAGS="-I/usr/local/opt/llvm/include"
+
 # doom-emacs
 export PATH="$HOME/.doom/bin:$PATH"
 
--- mUseUniversalHeaderFile.sh
+++ mUseUniversalHeaderFile.sh
@@ -1,4 +1,4 @@
 wget -P ~/Downloads https://gist.githubusercontent.com/chuxubank/378162ea500aa54738f1068c2dfa8933/raw/stdc++.h
-cd /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/
-sudo rsync ~/Downloads/stdc++.h bits/
+sudo rsync ~/Downloads/stdc++.h /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/bits/
+sudo rsync ~/Downloads/stdc++.h /usr/local/opt/llvm/include/c++/v1/bits/
 rm ~/Downloads/stdc++.h
