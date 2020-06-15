358aacec6c21e00dd471cdcde2c905a9b24dea99
Asuna <chuxubank@qq.com>
2020-06-15 08:27:19 +0800

    Remove unneeded config

--- .emacs-profiles.el
+++
@@ -1,5 +0,0 @@
-(("spacemacs" . ((user-emacs-directory . "~/.spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
- ("doom" . ((user-emacs-directory . "~/.doom")
-            (env . (("DOOMDIR" . "~/.doom.d")))))
- )
--- mLiberime.sh
+++
@@ -1,5 +0,0 @@
-cd $RIME_PATH
-git pull
-sudo cp /Library/Input\ Methods/Squirrel.app/Contents/Frameworks/librime.1.dylib $RIME_PATH/build/lib/Release/librime.dylib
-sudo cp /Library/Input\ Methods/Squirrel.app/Contents/Frameworks/librime.1.dylib /usr/local/lib
-echo "✅  librime updated"
